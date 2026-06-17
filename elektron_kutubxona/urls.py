3e88a932305997e8422b3b53a17f03ffaedce806
shadoliya09-maker <shadoliya09@gmail.com>
2026-06-17 11:52:38 +0500

    Fix ALLOWED_HOSTS for Render

--- elektron_kutubxona/urls.py
+++ elektron_kutubxona/urls.py
@@ -15,9 +15,8 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 urlpatterns = [
-
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
