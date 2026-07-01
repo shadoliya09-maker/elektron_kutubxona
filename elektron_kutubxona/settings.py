e8c458006fecd350db291fcda35b9c8a78c8d044
shadoliya09-maker <shadoliya09@gmail.com>
2026-07-01 11:25:10 +0500

    Fix ALLOWED_HOSTS for Render

--- elektron_kutubxona/settings.py
+++ elektron_kutubxona/settings.py
@@ -149,4 +149,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR = '/media/'
